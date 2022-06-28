f0609cdaa595a61e99a0c0b1b08ac2de7f706d95
xiaoqiyang <meetqy@qq.com>
2022-06-28 11:29:45 +0800

    gitignore

---
+++ .output/server/chunks/app/client.manifest.mjs
@@ -0,0 +1,24 @@
+const client_manifest = {
+  "node_modules/nuxt/dist/app/entry.mjs": {
+    "file": "entry-08902094.mjs",
+    "src": "node_modules/nuxt/dist/app/entry.mjs",
+    "isEntry": true,
+    "dynamicImports": [
+      "pages/[language]/card/1.vue"
+    ],
+    "css": [
+      "entry.96b5ed6f.css"
+    ]
+  },
+  "pages/[language]/card/1.vue": {
+    "file": "1-55cea316.mjs",
+    "src": "pages/[language]/card/1.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.mjs"
+    ]
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map

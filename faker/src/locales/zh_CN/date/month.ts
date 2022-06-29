9c6735ad62b39a43982b89e455955a1aa398bc56
xiaoqiyang <meetqy@qq.com>
2022-06-29 17:12:10 +0800

    晚上中文

---
+++ faker/src/locales/zh_CN/date/month.ts
@@ -0,0 +1,33 @@
+// Source: http://unicode.org/cldr/trac/browser/tags/release-27/common/main/en.xml#L1799
+export default {
+  wide: [
+    "一月",
+    "二月",
+    "三月",
+    "四月",
+    "五月",
+    "六月",
+    "七月",
+    "八月",
+    "九月",
+    "十月",
+    "十一月",
+    "十二月",
+  ],
+  // Property "wide_context" is optional, if not set then "wide" will be used instead
+  // It is used to specify a word in context, which may differ from a stand-alone word
+  wide_context: [
+    "一月",
+    "二月",
+    "三月",
+    "四月",
+    "五月",
+    "六月",
+    "七月",
+    "八月",
+    "九月",
+    "十月",
+    "十一月",
+    "十二月",
+  ],
+};
---
+++ faker/src/locales/zh_CN/date/weekday.ts
@@ -0,0 +1,15 @@
+// Source: http://unicode.org/cldr/trac/browser/tags/release-27/common/main/en.xml#L1847
+export default {
+  wide: ["星期天", "星期一", "星期二", "星期三", "星期三", "星期五", "星期六"],
+  // Property "wide_context" is optional, if not set then "wide" will be used instead
+  // It is used to specify a word in context, which may differ from a stand-alone word
+  wide_context: [
+    "星期天",
+    "星期一",
+    "星期二",
+    "星期三",
+    "星期三",
+    "星期五",
+    "星期六",
+  ],
+};

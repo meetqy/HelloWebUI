1db7d9612d21e8a4f264f6898c722c6bd29b1645
xiaoqiyang <meetqy@qq.com>
2022-07-02 15:40:18 +0800

    优化 copy template

--- composables/copy.ts
+++ composables/copy.ts
@@ -1,19 +1,22 @@
 export const createHtml = (str: string) => {
   return `<!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link
-          href="https://cdn.jsdelivr.net/npm/daisyui@2.17.0/dist/full.css"
-          rel="stylesheet"
-          type="text/css"
-        />
-        <script src="https://cdn.tailwindcss.com"></script>
-      </head>
-      <body>
-        ${str}
-      </body>
-    </html>
-    `;
+  <html>
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        href="https://cdn.jsdelivr.net/npm/daisyui@2.17.0/dist/full.css"
+        rel="stylesheet"
+        type="text/css"
+      />
+      <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body>
+      <!-- 更多响应式多主题模板: https://wcao.cc -->
+      <!-- 切换主题: <body data-theme='dark'> -->
+      <!-- 可用主题列表: https://daisyui.com/docs/themes/ -->
+      ${str}
+    </body>
+  </html>  
+`;
 };

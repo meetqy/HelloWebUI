d0af1f7102ef81f9dda4f441b3abbc073fb477a1
xiaoqiyang <meetqy@qq.com>
2022-07-20 13:44:24 +0800

    test build g

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,23 +21,6 @@
             <span class="!text-base-content uppercase">UI</span>
           </div>
 
-          <button class="btn btn-sm btn-ghost mr-2" @click="onCopy">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="2"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"
-              />
-            </svg>
-          </button>
-
           <div class="dropdown dropdown-end mr-2" v-if="props.lang">
             <label tabindex="0" class="btn btn-sm m-1 capitalize"
               >language</label
@@ -62,6 +45,23 @@
               </li>
             </ul>
           </div>
+
+          <button class="btn btn-sm btn-ghost mr-2" @click="onCopy">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"
+              />
+            </svg>
+          </button>
 
           <button class="btn btn-sm btn-ghost" @click="openFull">
             <svg
